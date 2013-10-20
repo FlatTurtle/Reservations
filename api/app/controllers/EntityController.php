7e99e0a534554c47da617c29424b5ef8efa1d2bd
Quentin Kaiser <kaiserquentin@gmail.com>
2013-10-20 14:37:21 +0200

    Use ISO4217 to validate currencies

--- api/app/controllers/EntityController.php
+++ api/app/controllers/EntityController.php
@@ -157,13 +157,24 @@ class EntityController extends Controller {
                     Validator::extend('price', function($attribute, $value, $parameters)
                     {
                         $timings = array('hourly', 'daily', 'weekly', 'monthly', 'yearly');
-                        $currencies = array('EUR', 'USD', 'YEN');
+                        $ISO4217 = array("AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT",
+"BGN","BHD","BIF","BMD","BND","BOB","BOV","BRL","BSD","BTN","BWP","BYR","BZD","CAD","CDF","CHE","CHF","CHW",
+"CLF","CLP","CNY","COP","COU","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR",
+"FJD","FKP","GBP","GEL","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","INR",
+"IQD","IRR","ISK","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR",
+"LRD","LSL","LTL","LVL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MXV",
+"MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON",
+"RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","SVC","SYP","SZL",
+"THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","USN","USS","UYI","UYU","UZS","VEF",
+"VND","VUV","WST","XAF","XAG","XAU","XBA","XBB","XBC","XBD","XCD","XDR","XFU","XOF","XPD","XPF","XPT","XSU",
+"XTS","XUA","XXX","YER","ZAR","ZMW","ZWL");
+
                         $intersect = array_intersect($timings, array_keys($value));
                         foreach($intersect as $index){
                             if($value[$index] < 0)
                                 return false;
                         }
-                        return (isset($value['currency']) && in_array($value['currency'], $currencies) && 
+                        return (isset($value['currency']) && in_array($value['currency'], $ISO4217) && 
                             !empty($intersect));
                     });
 
