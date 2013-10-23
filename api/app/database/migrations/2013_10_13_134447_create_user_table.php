e0abed5bac732795edb1ec4a42c08bc3a1be803a
Quentin Kaiser <kaiserquentin@gmail.com>
2013-10-24 00:58:54 +0200

    Forgot a migrate script

---
+++ api/app/database/migrations/2013_10_13_134447_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->string('username');
+			$table->string('password');
+			$table->integer('rights');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+
+		DB::table('user')->delete();
+		Schema::drop('user');
+
+	}
+
+}
