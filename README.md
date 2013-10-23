f9d911a2a712cd87ea21c06a86ff3163d1104ac1
Quentin Kaiser <kaiserquentin@gmail.com>
2013-10-23 15:52:34 +0200

    This is how I rollback

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
 reservations
 ============
 
-reservations api to reserve things (such as meeting rooms)
+Reservations api to reserve things (such as meeting rooms)
 
-
-Routes that needs authentication are using HTTP BasicAuth for now. 
 
 Requirements 
 =============
 
-PHP => 5.3
-MySQL => 5.5
+* PHP => 5.3
+* MySQL => 5.5
 
 How-to
 ======
 
-git clone git@github.com:FlatTurtle/Reservations.git
-cd Reservations
-php composer.phar install 
-cd api
-phpunit
+    git clone git@github.com:FlatTurtle/Reservations.git
+    cd Reservations
+    php composer.phar install 
+    cd api
+    phpunit
+
 
 ! You have to change values in config/testing/database.php to test the app
--- api/app/database/seeds/CustomerTableSeeder.php
+++ api/app/database/seeds/CustomerTableSeeder.php
@@ -1,22 +1,20 @@
 <?php
 
-use Hautelook\Phpass\PasswordHash;
+
 class CustomerTableSeeder extends Seeder {
 
 	public function run()
     {
-        $passwordHasher = new PasswordHash(8,false);
 		if(Schema::hasTable('customer'))
 	 		DB::table('customer')->delete();
-
         DB::table('customer')->insert(array(
             'username' => 'test',
-            'password' => $passwordHasher->HashPassword('test')
+            'password' => Hash::make('test')
         ));
 
         DB::table('customer')->insert(array(
             'username' => 'test2',
-            'password' => $passwordHasher->HashPassword('test')
+            'password' => Hash::make('test2')
         ));
     }
 }
