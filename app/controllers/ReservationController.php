e5960e304e72b46fc6b0dc463021f624dde50210
Nik Torfs <niktorfs@gmail.com>
2013-12-14 13:35:02 +0100

    quick dirty fix for $this problem @ server and adding access control header

--- app/controllers/ReservationController.php
+++ app/controllers/ReservationController.php
@@ -209,31 +209,31 @@ class ReservationController extends Controller
             if(!strcmp($clustername, Auth::user()->clustername) || Auth::user()->isAdmin()){
                 
                 Validator::extend('time', function($attribute, $value, $parameters)
-                                  {
-                                      if(!isset($value['from']) || !isset($value['to']))
-                                          return false;
-                                      //check against ISO8601 regex
-                                      if(!$this->isValidISO8601($value['from']))
-                                          return false;
-                                      if(!$this->isValidISO8601($value['to']))
-                                          return false;
-                                      $from=strtotime($value['from']);
-                                      $to=strtotime($value['to']);
-                                      $now = time();
-                                      $span = (int) Config::get('app.reservation_time_span');
+                {
+                    if(!isset($value['from']) || !isset($value['to']))
+                        return false;
+                    //check against ISO8601 regex
+                    if(!(preg_match('/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/', $value['from']) > 0))
+                        return false;
+                    if(!(preg_match('/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/', $value['to']) > 0))
+                        return false;
+                    $from=strtotime($value['from']);
+                    $to=strtotime($value['to']);
+                    $now = time();
+                    $span = (int) Config::get('app.reservation_time_span');
 
-                                      if(!$from || !$to)
-                                          return false;
-                                      if($from < $now - $span)
-                                          return false;
-                                      if ($to < $now - $span)
-                                          return false;
-                                      if ($to < $from)
-                                          return false;
-                                      if (($to-$from) < $span)
-                                          return false;
-                                      return true;
-                                  });
+                    if(!$from || !$to)
+                        return false;
+                    if($from < $now - $span)
+                        return false;
+                    if ($to < $now - $span)
+                        return false;
+                    if ($to < $from)
+                        return false;
+                    if (($to-$from) < $span)
+                        return false;
+                    return true;
+                });
 
                 $content = Request::instance()->getContent(); 
                 if (empty($content)) 
