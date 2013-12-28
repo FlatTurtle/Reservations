27b878c32650fa04da5326859b2146b3904c324b
Nik Torfs <niktorfs@gmail.com>
2013-12-28 15:28:06 +0100

    pre spring cleaning

--- app/controllers/EntityController.php
+++ app/controllers/EntityController.php
@@ -13,31 +13,25 @@ class EntityController extends BaseController {
      */
     public function getEntities($clustername) {
       
-        $cluster = Cluster::where('clustername', '=', $clustername)->first();
+        $cluster = $this->getCluster($clustername);
 
-        if (isset($cluster)) {
+        /* retrieve all entities from db and push their json bodies into an array
+           that we return to the user as json */
 
-            /* retrieve all entities from db and push their json bodies into an array
-               that we return to the user as json */
-
-            $_entities = Entity::where('user_id', '=', $cluster->user->id)->get()->toArray();
-            $entities = array();
-            $i = 0;
-            foreach ($_entities as $entity) {
-                if (isset($entity['body'])) {
-                    $entities[$i] = json_decode($entity['body']);
-                    if (is_null($entities[$i])) {
-                        $entities[$i] = new stdClass();
-                    }
-                    $entities[$i]->id = $entity['id'];
-                    $i++;
+        $_entities = Entity::where('user_id', '=', $cluster->user->id)->get()->toArray();
+        $entities = array();
+        $i = 0;
+        foreach ($_entities as $entity) {
+            if (isset($entity['body'])) {
+                $entities[$i] = json_decode($entity['body']);
+                if (is_null($entities[$i])) {
+                    $entities[$i] = new stdClass();
                 }
+                $entities[$i]->id = $entity['id'];
+                $i++;
             }
-            return Response::json($entities);
-                        
-        } else {
-            return $this->_sendErrorMessage(404, "Cluster.NotFound", "Cluster not found.");
         }
+        return Response::json($entities);
     }
 
     /**
@@ -47,28 +41,22 @@ class EntityController extends BaseController {
      */
     public function getAmenities($clustername) {
         
-        $cluster = Cluster::where('clustername', '=', $clustername)->first();
+        $cluster = $this->getCluster($clustername);
 
-        if (isset($cluster)) {
-
-            /* retrieve all entities with type 'amenity' 
-               from db and push their json bodies into an array
-               that we return to the user as json */
-            $amenities = Entity::where('user_id', '=', $cluster->user->id)
+        /* retrieve all entities with type 'amenity'
+           from db and push their json bodies into an array
+           that we return to the user as json */
+        $amenities = Entity::where('user_id', '=', $cluster->user->id)
             ->where('type', '=', 'amenity')
             ->get()
             ->toArray();
 
-            foreach ($amenities as $amenity) {
-                if (isset($amenity['body'])) {
-                    $amenity['body'] = json_decode($amenity['body']);        
-                } 
+        foreach ($amenities as $amenity) {
+            if (isset($amenity['body'])) {
+                $amenity['body'] = json_decode($amenity['body']);
             }
-            return Response::json($amenities);
-            
-        } else {
-            return $this->_sendErrorMessage(404, "Cluster.NotFound", "Cluster not found.");
         }
+        return Response::json($amenities);
     }
 
     /**
@@ -78,26 +66,18 @@ class EntityController extends BaseController {
      *
      */
     public function getAmenityByName($clustername, $name) {
+        $cluster = $this->getCluster($clustername);
+        $amenity = Entity::whereRaw(
+            'user_id = ? and type = ? and name = ?',
+            array($cluster->user->id, 'amenity', $name)
+        )->first();
 
-    
-        $cluster = Cluster::where('clustername', '=', $clustername)->first();
-
-        if (isset($cluster)) {
-            $amenity = Entity::whereRaw(
-                'user_id = ? and type = ? and name = ?',
-                array($cluster->user->id, 'amenity', $name)
-            )->first();
-
-            if (!isset($amenity)) {
-                return $this->_sendErrorMessage(404, "Amenity.NotFound", "Amenity not found.");
-            } else {
-                $d = json_decode($amenity->body);
-                $d->id = $amenity->id;
-                return Response::json($d);
-            }
-            
+        if (!isset($amenity)) {
+            return $this->_sendErrorMessage(404, "Amenity.NotFound", "Amenity not found.");
         } else {
-            return $this->_sendErrorMessage(404, "Cluster.NotFound", "Cluster not found.");
+            $d = json_decode($amenity->body);
+            $d->id = $amenity->id;
+            return Response::json($d);
         }
     }
 
@@ -110,23 +90,17 @@ class EntityController extends BaseController {
      */
     public function getEntityByName($clustername, $name) {
 
-        $cluster = Cluster::where('clustername', '=', $clustername)->first();
+        $cluster = $this->getCluster($clustername);
+        $entity = Entity::where('user_id', '=', $cluster->user->id)
+            ->where('name', '=', $name)
+            ->first();
 
-        if (isset($cluster)) {
-            $entity 
-                = Entity::where('user_id', '=', $cluster->user->id)
-                ->where('name', '=', $name)
-                ->first();
-            if (!isset($entity)) {
-                return $this->_sendErrorMessage(404, "Thing.NotFound", "Thing not found.");
-            } else {
-                $d = json_decode($entity->body);
-                $d->id = $entity->id;
-                return Response::json($d);
-            }
-            
+        if (!isset($entity)) {
+            return $this->_sendErrorMessage(404, "Thing.NotFound", "Thing not found.");
         } else {
-            return $this->_sendErrorMessage(404, "Cluster.NotFound", "Cluster not found.");
+            $d = json_decode($entity->body);
+            $d->id = $entity->id;
+            return Response::json($d);
         }
     }
 
@@ -138,62 +112,58 @@ class EntityController extends BaseController {
      */
     public function createEntity($clustername, $name) {
 
-        $cluster = Cluster::where('clustername', '=', $clustername)->first();
-        if (isset($cluster)) {
+        $cluster = $this->getCluster($clustername);
 
-            if (!strcmp($clustername, Auth::user()->clustername) || Auth::user()->isAdmin()) {
+        if (!strcmp($clustername, Auth::user()->clustername) || Auth::user()->isAdmin()) {
 
-                $content = Request::instance()->getContent(); 
-                if (empty($content)) 
-                  return $this->_sendErrorMessage(400, "Payload.Null", "Received payload is empty.");
-                if (Input::json() == null)
-                  return $this->_sendErrorMessage(400, "Payload.Invalid", "Received payload is invalid.");
-                
-                $room_validator = Validator::make(
-                    Input::json()->all(),
-                    array(
-                        'type' => 'required|alpha_dash',
-                        'body' => 'required|body'
-                    )
-                );
+            $content = Request::instance()->getContent();
+            if (empty($content))
+              return $this->_sendErrorMessage(400, "Payload.Null", "Received payload is empty.");
+            if (Input::json() == null)
+              return $this->_sendErrorMessage(400, "Payload.Invalid", "Received payload is invalid.");
 
-                // Validator testing
-                if (!$room_validator->fails()) {
-                    $body = Input::json()->get('body');
-                    $entity = Entity::where('name', '=', $body['name'])
-                        ->where('user_id', '=', $cluster->user->id)
-                        ->first();
+            $room_validator = Validator::make(
+                Input::json()->all(),
+                array(
+                    'type' => 'required|alpha_dash',
+                    'body' => 'required|body'
+                )
+            );
 
-                    if (isset($entity)) {
-                        // the entity already exist in db, we update the json body.
-                        $entity->body = json_encode($body);
-                        if($entity->save())
-                            return Response::json(
-                                array(
-                                  'success' => true,
-                                  'message' => 'Thing successfully updated.'
-                                )
-                            );
-                    } else {
-                        // the entity don't exist in db so we insert it.
-                        return Entity::create(
+            // Validator testing
+            if (!$room_validator->fails()) {
+                $body = Input::json()->get('body');
+                $entity = Entity::where('name', '=', $body['name'])
+                    ->where('user_id', '=', $cluster->user->id)
+                    ->first();
+
+                if (isset($entity)) {
+                    // the entity already exist in db, we update the json body.
+                    $entity->body = json_encode($body);
+                    if($entity->save())
+                        return Response::json(
                             array(
-                                'name' => $body['name'],
-                                'type' => Input::json()->get('type'),
-                                'body' => json_encode($body),
-                                'user_id' => $cluster->user->id
+                              'success' => true,
+                              'message' => 'Thing successfully updated.'
                             )
                         );
-                    }
                 } else {
-                    return $this->_sendValidationErrorMessage($room_validator);
-                } 
+                    // the entity don't exist in db so we insert it.
+                    return Entity::create(
+                        array(
+                            'name' => $body['name'],
+                            'type' => Input::json()->get('type'),
+                            'body' => json_encode($body),
+                            'user_id' => $cluster->user->id
+                        )
+                    );
+                }
             } else {
-                return $this->_sendErrorMessage(403, "WriteAccessForbiden", "You can't create things on behalf of another user.");
+                return $this->_sendValidationErrorMessage($room_validator);
             }
         } else {
-            return $this->_sendErrorMessage(404, "Cluster.NotFound", "Cluster not found.");
-        }   
+            return $this->_sendErrorMessage(403, "WriteAccessForbiden", "You can't create things on behalf of another user.");
+        }
     }
 
     /**
@@ -205,53 +175,48 @@ class EntityController extends BaseController {
     public function createAmenity($clustername, $name) {
 
         $content = Request::instance()->getContent(); 
-        if (empty($content)) 
+        if (empty($content)){
             return $this->_sendErrorMessage(400, "Payload.Null", "Received payload is empty.");
-        if (Input::json() == null)
+        }
+        if (Input::json() == null){
             return $this->_sendErrorMessage(400, "Payload.Invalid", "Received payload is invalid.");
-
+        }
         
-
-        
-        $cluster = Cluster::where('clustername', '=', $clustername)->first();
-        if (isset($cluster)) {
+        $cluster = $this->getCluster($clustername);
             
-            if (!strcmp($clustername, Auth::user()->clustername) || Auth::user()->isAdmin()) {
-                /* This Validator verify that the schema value is a valid json-schema
-                   definition. */
-                $amenity_validator = Validator::make(
-                    Input::json()->all(),
-                    array(
-                        'description' => 'required',
-                        'schema' => 'required|schema'
-                    )
-                );
+        if (!strcmp($clustername, Auth::user()->clustername) || Auth::user()->isAdmin()) {
+            /* This Validator verify that the schema value is a valid json-schema
+               definition. */
+            $amenity_validator = Validator::make(
+                Input::json()->all(),
+                array(
+                    'description' => 'required',
+                    'schema' => 'required|schema'
+                )
+            );
 
 
-                if (!$amenity_validator->fails()) {
-                    $amenity = Entity::where('name', '=', $name)->first();
-                    if (isset($amenity)) {
-                        $amenity->body = json_encode(Input::json()->get('schema'));
-                        $amenity->save();
-                    } else {
-                        return Entity::create(
-                            array(
-                                'name' => $name,
-                                'type' => 'amenity',
-                                'body' => json_encode(Input::json()->get('schema')),
-                                'user_id' => $cluster->user->id
-                            )
-                        );
-                    }
+            if (!$amenity_validator->fails()) {
+                $amenity = Entity::where('name', '=', $name)->first();
+                if (isset($amenity)) {
+                    $amenity->body = json_encode(Input::json()->get('schema'));
+                    $amenity->save();
                 } else {
-                    return $this->_sendValidationErrorMessage($amenity_validator);
+                    return Entity::create(
+                        array(
+                            'name' => $name,
+                            'type' => 'amenity',
+                            'body' => json_encode(Input::json()->get('schema')),
+                            'user_id' => $cluster->user->id
+                        )
+                    );
                 }
             } else {
-                return $this->_sendErrorMessage(403, "WriteAccessForbiden", "You can't create amenities on behalf of another user.");
+                return $this->_sendValidationErrorMessage($amenity_validator);
             }
         } else {
-            return $this->_sendErrorMessage(404, "Cluster.NotFound", "Cluster not found.");
-        }               
+            return $this->_sendErrorMessage(403, "WriteAccessForbiden", "You can't create amenities on behalf of another user.");
+        }
     }
 
     /**
@@ -261,37 +226,30 @@ class EntityController extends BaseController {
      *
      */
     public function deleteAmenity($clustername, $name) {
-        
-        
-        $cluster = Cluster::where('clustername', '=', $clustername)->first();
-        
-        if (isset($cluster)) {
 
-            if (!strcmp($clustername, Auth::user()->clustername) || Auth::user()->isAdmin()) {
-                
-                $amenity = Entity::where('user_id', '=', $cluster->user->id)
-                    ->where('type', '=', 'amenity')
-                    ->where('name', '=', $name);
+        $cluster = $this->getCluster($clustername);
 
-                if ($amenity->first() != null)
-                    if($amenity->delete())
-                        return Response::json(
-                            array(
-                              'success' => true,
-                              'message' => 'Amenity successfully deleted'
-                            )
-                        );
-                    else
-                        return $this->_sendErrorMessage(500, "Amenity.Unknown", "An error occured while deleting the amenity.");
+        if (!strcmp($clustername, Auth::user()->clustername) || Auth::user()->isAdmin()) {
+
+            $amenity = Entity::where('user_id', '=', $cluster->user->id)
+                ->where('type', '=', 'amenity')
+                ->where('name', '=', $name);
+
+            if ($amenity->first() != null)
+                if($amenity->delete())
+                    return Response::json(
+                        array(
+                          'success' => true,
+                          'message' => 'Amenity successfully deleted'
+                        )
+                    );
                 else
-                    return $this->_sendErrorMessage(404, "Amenity.NotFound", "Amenity not found.");
-                    
-            } else {
-                return $this->_sendErrorMessage(403, "DeleteAccessForbiden", "You can't delete amenities from another user.");
-            }
-            
+                    return $this->_sendErrorMessage(500, "Amenity.Unknown", "An error occured while deleting the amenity.");
+            else
+                return $this->_sendErrorMessage(404, "Amenity.NotFound", "Amenity not found.");
+
         } else {
-            return $this->_sendErrorMessage(404, "Cluster.NotFound", "Cluster not found.");
+            return $this->_sendErrorMessage(403, "DeleteAccessForbiden", "You can't delete amenities from another user.");
         }
     }
 }
