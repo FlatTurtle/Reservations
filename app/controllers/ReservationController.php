f30c711533b151506e1e0b825d392d6633bb1ec4
Nik Torfs <niktorfs@gmail.com>
2014-01-03 00:51:54 +0100

    included checks for unactivated reservations and a 30 minute blocking time of unactivated reservations

--- app/controllers/ReservationController.php
+++ app/controllers/ReservationController.php
@@ -11,88 +11,88 @@
 class ReservationController extends BaseController
 {
 
-     /**
-     * Verify if a room is available by checking its $opening_hours
-     * agains the $reservation_time.
-     * @param opening_hours : the room's opening hours
-     * @param reservation_time : the reservation's time (from & to)
-     */
-      private function isAvailable($opening_hours, $reservation_time)
-      {
+    /**
+    * Verify if a room is available by checking its $opening_hours
+    * agains the $reservation_time.
+    *
+    * @param opening_hours : the room's opening hours
+    * @param reservation_time : the reservation's time (from & to)
+    *
+    * @return true if the room is open at the given $reservation_time
+    */
+    private function isAvailable($opening_hours, $reservation_time)
+    {
 
-          $from = strtotime($reservation_time['from']);
-          $to = strtotime($reservation_time['to']);
-          $available = false;
-          foreach ($opening_hours as $opening_hour) {
+        $from = strtotime($reservation_time['from']);
+        $to = strtotime($reservation_time['to']);
+        $available = false;
+        foreach ($opening_hours as $opening_hour) {
 
-              /* 
-               * We do not support reservation that goes on multiple days,
-               * if a user wants to book an entity on multiple days he had to
-               * do reservations for each day
-               */
+            /*
+            * We do not support reservation that goes on multiple days,
+            * if a user wants to book an entity on multiple days he had to
+            * do reservations for each day
+            */
 
-              //compare dayOfWeek with the day value of $from and $to
-              if ($opening_hour->dayOfWeek == date('N', $from)
-                  && $opening_hour->dayOfWeek == date('N', $to)
-              ) {
-                  // check validdate after confirming the dayofweek is right
-                  // valid date can differ each opening interval
-                  if ($from < strtotime($opening_hour->validFrom)) {
-                      return false;
-                  }
-                  if ($to > strtotime($opening_hour->validThrough)) {
-                      return false;
-                  }
+            //compare dayOfWeek with the day value of $from and $to
+            if ($opening_hour->dayOfWeek == date('N', $from)
+                && $opening_hour->dayOfWeek == date('N', $to)) {
 
-                  $from_t = new DateTime();
-                  $from_t->setTimestamp($from);
-                  $from_open_t = clone $from_t;
-                  $from_close_t = clone $from_t;
-                  $to_t = new DateTime();
-                  $to_t->setTimestamp($to);
-                  $to_open_t = clone $to_t;
-                  $to_close_t = clone $to_t;
+                // check validate after confirming the dayofweek is right
+                // valid date can differ each opening interval
+                if ($from < strtotime($opening_hour->validFrom)) {
+                  return false;
+                }
+                if ($to > strtotime($opening_hour->validThrough)) {
+                  return false;
+                }
 
-                  foreach (array_combine($opening_hour->opens, $opening_hour->closes) 
-                    as $open => $close) {
-                      $i=0;
+                $from_t = new DateTime();
+                $from_t->setTimestamp($from);
+                $to_t = new DateTime();
+                $to_t->setTimestamp($to);
 
-                      //we parse hour time
-                      $open = explode(':', $open);
-                      $open_hours = intval($open[0]);
-                      $open_minutes = intval($open[1]);
+                // using the date part to later on change the time
+                // clone to not have a reference
+                $open_t = clone $from_t;
+                $close_t = clone $from_t;
 
-                      //we parse closing time
-                      $close = explode(':', $close);
-                      $close_hours = intval($close[0]);
-                      $close_minutes = intval($close[1]);
+                foreach (array_combine($opening_hour->opens, $opening_hour->closes)
+                as $open => $close) {
 
-                      //building opening and closing time for requested day
-                      $from_open_t->setTime($open_hours, $open_minutes);
-                      $from_close_t->setTime($close_hours, $close_minutes);
-                      $to_open_t->setTime($open_hours, $open_minutes);
-                      $to_close_t->setTime($close_hours, $close_minutes);
+                    //we parse hour time
+                    $open = explode(':', $open);
+                    $open_hours = intval($open[0]);
+                    $open_minutes = intval($open[1]);
 
-                      if ($from_t >= $from_open_t)
-                        $i++;
-                      if ($from_t < $from_close_t)
-                          $i--;
-                      if ($to_t > $to_open_t)
-                        $i++; 
-                      if ($to_t <= $to_close_t)
-                        $i--;
-                      if (!$i) $available=true;
-                  }
-                  
-              }
-          }
-          return $available;
-      }
+                    //we parse closing time
+                    $close = explode(':', $close);
+                    $close_hours = intval($close[0]);
+                    $close_minutes = intval($close[1]);
+
+                    //building opening and closing time for requested day
+                    $open_t->setTime($open_hours, $open_minutes);
+                    $close_t->setTime($close_hours, $close_minutes);
+
+                    if ($from_t >= $open_t
+                        && $from_t < $close_t
+                        && $to_t > $open_t
+                        && $to_t <= $close_t){
+
+                        $available=true;
+                    }
+                }
+
+            }
+        }
+        return $available;
+    }
 
 
     /**
      * Return a list of reservations that the user has made for the current day.
      * Day can be change by providing a 'day' as GET parameter.
+     * This only returns activated reservations
      *
      * @param clustername : the cluster's name
      * @return 
@@ -102,48 +102,41 @@ class ReservationController extends BaseController
         /*  Announce value is json encoded in db so we first retrieve
             reservations from db, decode announce json and return
             reservations to the user */
-        if (Input::get('day')!=null) {
-            $from = new DateTime(Input::get('day'));
-        }
-        else {
-            $from = new DateTime();
-            $from->setTime(0,0);
-        }
-        $from->setTimezone(new DateTimeZone('UTC'));
+        $from = $this->getDayFromInput();
+        $to = clone $from;
+        // add one day to $from
+        $to->add(new DateInterval('P1D'));
 
-        $_reservations = DB::select(
-            'select * from reservation where user_id = ?
-            AND UNIX_TIMESTAMP(`from`) >= ?
-            AND UNIX_TIMESTAMP(`to`) <= ?',
-            array($cluster->user->id, $from->getTimestamp(), $from->getTimestamp()+(60*60*24))
-        );
+        $_reservations = Reservation::activatedOrBlocking()
+            ->where('user_id', '=', $cluster->user->id)
+            ->where('from', '>=', $from)
+            ->where('to', '<=', $to)
+            ->get();
 
         //FIXME : return entity name instead of id ?
         $reservations = array();
         foreach($_reservations as $reservation){
-            $reservation->from = date('c', strtotime($reservation->from));
-            $reservation->to = date('c', strtotime($reservation->to));
             $reservation->announce = json_decode($reservation->announce);
             $reservation->customer = json_decode($reservation->customer);
-            array_push($reservations, $reservation);
+            array_push($reservations, $reservation->toArray());
         }
-        return Response::json($reservations);
+        return $reservations;
     }
 
     /**
      * Return a the reservation that has id $id.
+     * This only returns activated reservations
      * @param clustername : the cluster's name
-     * @param id : the id of the reservation to be deleted
+     * @param id : the id of the reservation to be returned
      */
     public function getReservation(Cluster $cluster, $id)
     {
         //TODO the cluster is not used. So as long as a valid cluster is given every reservation in the whole api can be returned
-        $reservation = Reservation::find($id);
+        $reservation = Reservation::activatedOrBlocking()->find($id);
         if(isset($reservation)) {
-            $reservation->from = date('c', strtotime($reservation->from));
-            $reservation->to = date('c', strtotime($reservation->to));
+            $reservation->announce = json_decode($reservation->customer);
             $reservation->customer = json_decode($reservation->customer);
-            return Response::json($reservation);
+            return $reservation;
         } else {
           return $this->_sendErrorMessage(404, "Reservation.NotFound", "Reservation not found");
         }
@@ -152,6 +145,8 @@ class ReservationController extends BaseController
 
     /**
      * Return a the reservation that has id $id.
+     * This only returns activated reservations
+     *
      * @param clustername : the user's name
      * @param name : the thing's name
      */
@@ -160,32 +155,25 @@ class ReservationController extends BaseController
         $thing = Entity::where('user_id', '=', $cluster->user->id)->where('name', '=', $name)->first();
         if (isset($thing)) {
 
-            if (Input::get('day')!=null) {
-                $from = new DateTime(Input::get('day'));
-            }
-            else {
-                $from = new DateTime();
-                $from->setTime(0,0);
-            }
-            $from->setTimezone(new DateTimeZone('UTC'));
+            $from = $this->getDayFromInput();
+            $to = clone $from;
+            // add one day to $from
+            $to->add(new DateInterval('P1D'));
 
-            $_reservations = DB::select(
-                'select * from reservation where user_id = ?
-                AND entity_id = ?
-                AND UNIX_TIMESTAMP(`from`) > ?
-                AND UNIX_TIMESTAMP(`to`) < ?',
-                array($cluster->user->id, $thing->id, $from->getTimestamp(), $from->getTimestamp()+(60*60*24))
-            );
+            $_reservations = Reservation::activatedOrBlocking()
+                ->where('user_id', '=', $cluster->user->id)
+                ->where('entity_id', '=', $thing->id)
+                ->where('from', '>=', $from)
+                ->where('to', '<=', $to)
+                ->get();
 
             $reservations = array();
             foreach($_reservations as $reservation){
-                $reservation->from = date('c', strtotime($reservation->from));
-                $reservation->to = date('c', strtotime($reservation->to));
                 $reservation->announce = json_decode($reservation->announce);
                 $reservation->customer = json_decode($reservation->customer);
-                array_push($reservations, $reservation);
+                array_push($reservations, $reservation->toArray());
             }
-            return Response::json($reservations);
+            return $reservations;
         } else{
             return $this->_sendErrorMessage(404, "Thing.NotFound", "Thing not found");
         }
@@ -245,16 +233,19 @@ class ReservationController extends BaseController
                         $from->setTimezone(new DateTimeZone('UTC'));
                         $to->setTimezone(new DateTimeZone('UTC'));
 
-                        $reservation = DB::table('reservation')
+                        $reservation = Reservation::activatedOrBlocking()
                             ->where('user_id', '=', $cluster->user->id)
                             ->where('entity_id', '=', $thing->id)
                             ->where('from', '<', $to)
                             ->where('to', '>', $from)
-                            ->get();
+                            ->first();
 
                         if(!empty($reservation)){
                             return $this->_sendErrorMessage(404, "Thing.AlreadyReserved", "The thing is already reserved at that time.");
                         }else{
+                            // TODO plug in code to send a confirmation mail
+                            // without the confirmation it will not be activated
+
                             return Reservation::create(
                                 array(
                                     'from' => $from->getTimestamp(),
@@ -265,6 +256,7 @@ class ReservationController extends BaseController
                                     'customer' => json_encode(Input::json()->get('customer')),
                                     'entity_id' => $thing->id,
                                     'user_id' => $cluster->user->id,
+                                    'activated' => false
                                 )
                             );
                         }
@@ -335,12 +327,12 @@ class ReservationController extends BaseController
                         $from->setTimezone(new DateTimeZone('UTC'));
                         $to->setTimezone(new DateTimeZone('UTC'));
 
-                        $reservation = DB::table('reservation')
-                          ->where('user_id', '=', $cluster->user->id)
-                          ->where('entity_id', '=', $entity->id)
-                          ->where('from', '<', $to)
-                          ->where('to', '>', $from)
-                          ->get();
+                          $reservation = Reservation::activatedOrBlocking()
+                              ->where('user_id', '=', $cluster->user->id)
+                              ->where('entity_id', '=', $entity->id)
+                              ->where('from', '<', $to)
+                              ->where('to', '>', $from)
+                              ->first();
 
                         if(!empty($reservation)){
                             return $this->_sendErrorMessage(404, "Thing.AlreadyReserved", "The thing is already reserved at that time.");
@@ -385,6 +377,18 @@ class ReservationController extends BaseController
         }else{
             return $this->_sendErrorMessage(404, "Reservation.NotFound", "Reservation not found.");
         }
+    }
+
+    private function getDayFromInput()
+    {
+        if (Input::get('day') != null) {
+            $from = new DateTime(Input::get('day'));
+        } else {
+            $from = new DateTime();
+            $from->setTime(0, 0);
+        }
+        $from->setTimezone(new DateTimeZone('UTC'));
+        return $from;
     }
 }
 
