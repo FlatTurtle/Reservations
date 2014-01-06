9a54b2678695eec1e9e06ab8016b525eebef1e6b
Jens Segers <segers.jens@gmail.com>
2014-01-06 14:33:18 +0100

    Better html

--- app/views/confirmed.blade.php
+++ app/views/confirmed.blade.php
@@ -2,8 +2,30 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" href="{{ URL::asset('assets/bootstrap.min.css') }}" type="text/css"/>
+		<link type="text/css" rel="stylesheet" href="https://fast.fonts.com/cssapi/66253153-9c89-413c-814d-60d3ba0d6ac2.css"/>
+		<style>
+		body {
+			font-family: "Soho Gothic W01 Light","Helvetica Neue",Helvetica,Arial,sans-serif;
+			text-align: center;
+		}
+		strong {
+			font-family: "Soho Gothic W01 Regular","Helvetica Neue",Helvetica,Arial,sans-serif;
+		}
+		h1 {
+			font-family: inherit;
+			font-weight: 300;
+			margin-top: 10%;
+		}
+		h2 {
+			margin-top: 20px;
+			font-weight: 300;
+			color: #999;
+		}
+		</style>
 	</head>
 	<body>
-		Reservation confirmed
+		<h1>You have successfully confirmed your reservation.</h1>
+		<h2>You can now close this window.</h2>
 	</body>
 </html>
