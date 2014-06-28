b0427125b5924b47055af911c467625ce63d340f
Yeri Tiete <yeri@flatturtle.com>
2014-06-28 21:03:44 +0200

    minor text change

--- app/views/emails/confirm.blade.php
+++ app/views/emails/confirm.blade.php
@@ -12,15 +12,16 @@
 			Meeting room: {{ $thing_name }} <br />
 			Date: {{ $from }} => {{ $to }} <br />
 			<br />
-			<strong>Confirm</strong> booking/Confirmez reservation/Bevestigen boeking: <br />
+			<strong>Confirm</strong> booking / Confirmez reservation / Bevestigen boeking: <br />
 			<a href="{{ URL::to($confirm_url) }}">{{ URL::to($confirm_url) }}</a><br />
  			<br />
-			<strong>Cancel</strong>/Annulez/Annuleer: <br />
+			<strong>Cancel</strong> / Annulez / Annuleer: <br />
 			<a href="{{ URL::to($cancel_url) }}">{{ URL::to($cancel_url) }}</a><br />
 			<br />
 			<br />
 			<br />
-			<p>help@FlatTurtle.com - &copy; <a href="http://FlatTurtle.com/">FlatTurtle</a></p>
+			<p>help@FlatTurtle.com <br />
+			&copy; <a href="http://FlatTurtle.com/">FlatTurtle</a></p>
 		</div>
 	</body>
 </html>
