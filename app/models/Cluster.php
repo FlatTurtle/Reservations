6805b75fff6e0b014edc33ae96bdde78e6b1d09e
= <niktorfs@gmail.com>
2014-10-19 15:41:34 +0200

    fixes #49

--- app/models/Cluster.php
+++ app/models/Cluster.php
@@ -59,5 +59,20 @@ class Cluster extends Eloquent implements UserInterface {
     public function isAdmin() {
         return $this->user()->first()->isAdmin();
     }
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
 }
 
